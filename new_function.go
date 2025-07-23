13b2f02f0636309e2f19962db3455d5c5933e72c
Nana Amoako <nanaamoako202@gmail.com>
2025-07-23 15:46:45 -0400

    moreee knowledge

---
+++ new_function.go
@@ -0,0 +1,19 @@
+package main
+
+// import "fmt"
+
+func concat(s1 string, s2 string) string {
+	return s1 + s2
+}
+
+// An idiomatic way to write the above function in Go is to use a single type for both parameters:
+
+// func concat(s1, s2 string) string {
+// 	return s1 + s2
+// }
+
+// func main() {
+// 	fmt.Println(concat("Hello, ", "World!"))
+// 	fmt.Println(concat("Nana ", "Amoako"))
+// }
+
--- password.go
+++ password.go
@@ -18,14 +18,30 @@ func password() {
 	}
 }
 
+// you can also use len for counting length in go
+// =: assignment operator
+// ==: equality operator
+// !=: not equal operator
+// >: greater than operator
+// <: less than operator
+// >=: greater than or equal to operator
+// <=: less than or equal to operator
+// &&: logical AND operator
+// ||: logical OR operator
+// !: logical NOT operator
+// functions in Go are first class citizens
+// meaning they can be passed as arguments to other functions
+// and can be returned from other functions
+
+
 // okay so functons can be declared anywhere in the file
 // and they can be called even after a function is defined
 
 func getLength(s string) int{
 	return len(s)
 }
-func main(){
-	fmt.Println("----------")
-	password()
-	fmt.Println("----------")
-}
+// func main(){
+// 	fmt.Println("----------")
+// 	password()
+// 	fmt.Println("----------")
+// }
